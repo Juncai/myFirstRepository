fbe1c3fd2793e6d1226a582984ddbc93fccc40c3
Alex_TJ_Chen <Alex_TJ_Chen@10.103.46.125>
2014-01-09 14:52:22 -0500

    V N

--- Test1/src/Test1/TestClass.java
+++ Test1/src/Test1/TestClass.java
@@ -3,5 +3,6 @@ package Test1;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world! This is DiaoCode led by Jiaoshen.");
+		System.out.println("Hello world! This is DiaoCode led by Jiaoshen.");
 	}
 }
